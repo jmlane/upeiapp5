5d1ae5be209ddfa72362fe6a28043b763c8ec81d
Jonathan M. Lane <jmlane@makeit.ca>
2012-02-28 00:57:56 -0400

    Committing code from assignment 2.

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
-
+from google.appengine.ext.webapp import template
+import re
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+
+        template_values = {}
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+class Fig35_1718Handler(webapp.RequestHandler):
+    def get(self):
+
+        template_values = {}
+
+        path = os.path.join(os.path.dirname(__file__), 'fig35_1718.html')
+        self.response.out.write(template.render(path, template_values))
+
+    def post(self):
+        # the regular expression for matching most US phone numbers.
+        telephoneExpression = re.compile(r'^\(\d{3}\)\d{3}-\d{4}$')
+
+        template_values = {
+                    'firstName' : self.request.get('firstname'),
+                    'lastName' : self.request.get('lastname'),
+                    'email' : self.request.get('email'),
+                    'phone' : self.request.get('phone'),
+                    'book' : self.request.get('book'),
+                    'os' : self.request.get('os')
+        }
+
+        for key, value in template_values.iteritems():
+            if value == '':
+                template_values['printFormError'] = True
+                break
+
+        if not telephoneExpression.match(template_values['phone']):
+            template_values['printPhoneError'] = True
+
+        path = os.path.join(os.path.dirname(__file__), 'fig35_1718_post.html')
+        self.response.out.write(template.render(path, template_values))
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
+    application = webapp.WSGIApplication([
+        ('/', MainHandler),
+        ('/fig35_1718', Fig35_1718Handler)],
+        debug=True)
     util.run_wsgi_app(application)
 
 
