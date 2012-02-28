91b3472dc401a93b5af297cff5cbc2eab4fe143c
Jonathan M. Lane <jmlane@makeit.ca>
2012-02-28 02:07:39 -0400

    Moved templates into dir & made index.html static.

--- main.py
+++ main.py
@@ -5,21 +5,15 @@ from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 import re
 
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-
-        template_values = {}
-
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
-
+TEMPLATE_DIR = 'templates'
 
 class Fig35_1718Handler(webapp.RequestHandler):
     def get(self):
 
         template_values = {}
 
-        path = os.path.join(os.path.dirname(__file__), 'fig35_1718.html')
+        path = os.path.join(os.path.dirname(__file__), TEMPLATE_DIR,
+                            'fig35_1718.html')
         self.response.out.write(template.render(path, template_values))
 
     def post(self):
@@ -43,15 +37,14 @@ class Fig35_1718Handler(webapp.RequestHandler):
         if not telephoneExpression.match(template_values['phone']):
             template_values['printPhoneError'] = True
 
-        path = os.path.join(os.path.dirname(__file__), 'fig35_1718_post.html')
+        path = os.path.join(os.path.dirname(__file__), TEMPLATE_DIR,
+                            'fig35_1718_post.html')
         self.response.out.write(template.render(path, template_values))
 
 
 def main():
-    application = webapp.WSGIApplication([
-        ('/', MainHandler),
-        ('/fig35_1718', Fig35_1718Handler)],
-        debug=True)
+    application = webapp.WSGIApplication([('/fig35_1718', Fig35_1718Handler)],
+                                         debug=True)
     util.run_wsgi_app(application)
 
 
