8c86a3fa8a9f4a3e49d0f17666b37b7e3e84653d
Jonathan M. Lane <jmlane@makeit.ca>
2012-04-04 19:57:18 -0300

    Committing work for assignment 3. Needs cleanup.

--- main.py
+++ main.py
@@ -4,53 +4,96 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 import re
+from models.models import MailingList
+from google.appengine.ext import db
+import urllib
 
 class MainHandler(webapp.RequestHandler):
+    '''Handler for landing page of application.'''
     def get(self):
-
-        template_values = {}
-
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
+        self.response.out.write(template.render(path, {}))
 
+class ListFormHandler(webapp.RequestHandler):
+    '''Handler for the mailing list registration form page.'''
 
-class Fig35_1718Handler(webapp.RequestHandler):
+    @staticmethod
+    def valid_form(form_dict):
+        for key, value in form_dict.iteritems():
+            if value == '':
+                return False
+        return True
+
     def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'form.html')
+        self.response.out.write(template.render(path, {}))
 
-        template_values = {}
-
-        path = os.path.join(os.path.dirname(__file__), 'fig35_1718.html')
-        self.response.out.write(template.render(path, template_values))
 
     def post(self):
         # the regular expression for matching most US phone numbers.
         telephoneExpression = re.compile(r'^\(\d{3}\)\d{3}-\d{4}$')
 
         template_values = {
-                    'firstName' : self.request.get('firstname'),
-                    'lastName' : self.request.get('lastname'),
+                    'first_name' : self.request.get('first_name'),
+                    'last_name' : self.request.get('first_name'),
                     'email' : self.request.get('email'),
                     'phone' : self.request.get('phone'),
                     'book' : self.request.get('book'),
+                    'bookurl' : urllib.quote(self.request.get('book')),
                     'os' : self.request.get('os')
         }
 
-        for key, value in template_values.iteritems():
-            if value == '':
-                template_values['printFormError'] = True
-                break
-
-        if not telephoneExpression.match(template_values['phone']):
+        if not self.valid_form(template_values):
+            template_values['printFormError'] = True
+        elif not telephoneExpression.match(template_values['phone']):
             template_values['printPhoneError'] = True
+        else:
+            form = MailingList(
+                    first_name = self.request.get('first_name'),
+                    last_name = self.request.get('last_name'),
+                    email = self.request.get('email'),
+                    phone = self.request.get('phone'),
+                    book = self.request.get('book'),
+                    os = self.request.get('os'))
+            form.put()
 
-        path = os.path.join(os.path.dirname(__file__), 'fig35_1718_post.html')
+        path = os.path.join(os.path.dirname(__file__), 'register.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+class ShowListHandler(webapp.RequestHandler):
+    '''Provides Handler for showlist page.'''
+    def get(self):
+        book = self.request.get('book')
+        template_values = {}
+
+        if not book == '':
+            query = db.Query(MailingList)
+            query.filter('book =', book)
+            template_values['book'] = book
+            template_values['results'] = [result for result in query]
+
+        path = os.path.join(os.path.dirname(__file__), 'showlist.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+class ShowAllHandler(webapp.RequestHandler):
+    '''Provides handler for the showall page.'''
+    def get(self):
+        query = db.Query(MailingList)
+
+        template_values = { 'results': [result for result in query] }
+
+        path = os.path.join(os.path.dirname(__file__), 'showall.html')
         self.response.out.write(template.render(path, template_values))
 
 
 def main():
     application = webapp.WSGIApplication([
         ('/', MainHandler),
-        ('/fig35_1718', Fig35_1718Handler)],
+        ('/form.html', ListFormHandler),
+        ('/showlist.html', ShowListHandler),
+        ('/showall.html', ShowAllHandler)],
         debug=True)
     util.run_wsgi_app(application)
 
--- models/models.py
+++ models/models.py
@@ -3,20 +3,20 @@ from google.appengine.ext import db
 
 class MailingList(db.Model):
     '''Defines the HRD Model for the assignment 2 mailing list.'''
-    first_name = db.stringProperty(required=True)
-    last_name = db.stringProperty(required=True)
+    first_name = db.StringProperty(required=True)
+    last_name = db.StringProperty(required=True)
     email = db.EmailProperty(required=True)
     phone = db.PhoneNumberProperty(required=True)
-    book = db.stringProperty(required=True, choices=set(
+    book = db.StringProperty(required=True, choices=set(
         ['XML How to Program',
          'Python How to Program',
          'E-business and E-commerce How to Program',
          'Internet and WWW How to Program 3e',
          'C++ How to Program 4e',
-         'How to Program 5e',
-         'Basic How to Program'
+         'Java How to Program 5e',
+         'Visual Basic How to Program'
         ]))
-    os = db.stringProperty(required=True, choices=set(
+    os = db.StringProperty(required=True, choices=set(
         ['Windows XP',
          'Windows 2000',
          'Windows 95_98',
