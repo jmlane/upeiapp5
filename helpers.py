40ea0dbd7f7c393a1ff090f7e10e0b9f911963d4
Jonathan M. Lane <jmlane@makeit.ca>
2012-04-24 17:48:55 -0300

    Corrected render helper function.

--- helpers.py
+++ helpers.py
@@ -4,5 +4,5 @@ from google.appengine.ext.webapp import template
 
 def render(page, template_values={}):
     '''Nice wrapper function for GAE template.render method.'''
-    path = os.path.join(os.path.dirname(__file__), 'templates/', page)
+    path = os.path.join(os.path.dirname(__file__), 'templates', page)
     return template.render(path, template_values)
