632baa8684121fbb68260845b7cab6fd97083c61
Jonathan M. Lane <jmlane@makeit.ca>
2012-04-24 17:50:21 -0300

    Altered model naming scheme.

--- models.py
+++ models.py
@@ -1,26 +1,27 @@
 from google.appengine.ext import db
 
-class Users(db.Model):
+class User(db.Model):
     name = db.StringProperty(required=True)
     email = db.EmailProperty(required=True)
     created_on = db.DateProperty(auto_now_add=True)
 
-class TwitterAccounts(db.Model):
+class TwitterAccount(db.Model):
     screen_name = db.StringProperty(required=True)
     id = db.IntegerProperty(required=True)
     name = db.StringProperty()
-    description = db.StringProperty()
-    location = db.StringProperty()
+    description = db.StringProperty(multiline=True)
+    location = db.StringProperty(multiline=True)
     url = db.LinkProperty()
 
     @property
     def mentions(self):
-        return Tweets.filter('mentions =', self.key())
+        return Tweet.filter('mentions =', self.key())
 
-class Tweets(db.Model):
-    author = db.ReferenceProperty(TwitterAccounts,
+class Tweet(db.Model):
+    author = db.ReferenceProperty(TwitterAccount,
                                   collection_name='tweets')
-    body = db.StringProperty(required=True)
+    text = db.StringProperty(required=True)
     in_reply_to = db.SelfReferenceProperty(collection_name='replies')
     mentions = db.ListProperty(db.Key)
+    created_at = db.DateProperty()
     geo = db.GeoPtProperty()
